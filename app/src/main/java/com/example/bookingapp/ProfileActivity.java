c5372ed5503621e8b6dcf042454d2aedc61ea11e
Tony <53233403+Tonynganga@users.noreply.github.com>
2021-09-04 10:42:19 +0300

    Additional Activities

---
+++ app/src/main/java/com/example/bookingapp/ProfileActivity.java
@@ -0,0 +1,24 @@
+package com.example.bookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.example.bookingapp.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
