d95a1d2c3102fd9ac68b7ab5df51dd2f172c328b
Christine Bai <bai20726@gmail.com>
2021-09-08 09:45:18 +0300

    about us page

---
+++ app/src/main/java/com/example/bookingapp/CancelTicket.java
@@ -0,0 +1,15 @@
+package com.example.bookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CancelTicket extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cancel_ticket);
+    }
+}
